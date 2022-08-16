d4546165df61181ce5a56a83b76dc51667e5bbb3
Li Jin <dragon-fly@qq.com>
2022-08-16 16:32:23 +0800

    sync.

--- spec/parser/parser_spec.lua
+++ spec/parser/parser_spec.lua
@@ -1,37 +1,99 @@
 local tl = require("tl")
 
-describe("parser errors", function()
-   it("parse errors include filename", function ()
-      local result = tl.process_string("local x 1", false, nil, "foo.tl")
-      assert.same("foo.tl", result.syntax_errors[1].filename, "parse errors should contain .filename property")
-   end)
-
-   it("parse errors in a required package include filename of required file", function ()
-      local io_open = io.open
-      finally(function() io.open = io_open end)
-      io.open = function (filename, mode)
-         if string.match(filename, "bar.tl$") then
-            -- Return a stub file handle
-            return {
-               read = function (_, format)
-                  if format == "*a" then
-                     return "local x 1"                  -- Return fake bar.tl content
-                  else
-                     error("Not implemented!")  -- Implement other modes if needed
-                  end
-               end,
-               close = function () end,
-            }
-         else
-            return io_open(filename, mode)
-         end
+local function strip_typeids(t)
+   local copy = {}
+   for k,v in pairs(t) do
+      if type(v) == "table" then
+         copy[k] = strip_typeids(v)
+      elseif k ~= "typeid" then
+         copy[k] = v
       end
+   end
+   return copy
+end
 
-      local code = [[
-         local bar = require "bar"
-      ]]
-      local result = tl.process_string(code, true, nil, "foo.tl")
-      assert.is_not_nil(string.match(result.env.loaded["bar.tl"].syntax_errors[1].filename, "bar.tl$"), "errors should contain .filename property")
+describe("parser", function()
+   it("accepts an empty file (regression test for #43)", function ()
+      local result = tl.process_string("")
+      assert.same({}, result.syntax_errors)
+      assert.same({
+         kind = "statements",
+         tk = "$EOF$",
+         type = {
+            typename = "none",
+         },
+         x = 1,
+         y = 1,
+         xend = 5,
+         yend = 1,
+      }, strip_typeids(result.ast))
    end)
-end)
 
+   it("accepts 'return;' (regression test for #52)", function ()
+      local result = tl.process_string("return;")
+      assert.same({}, result.syntax_errors)
+      assert.same(1, #result.ast)
+      assert.same("return", result.ast[1].kind)
+   end)
+
+   it("accepts semicolons in tables (regression test for #54)", function ()
+      local result = tl.process_string([[
+         local t = {
+            foo = "bar";
+            foo = "baz";
+         }
+      ]])
+      assert.same({}, result.syntax_errors)
+      assert.same(1, #result.ast)
+      assert.same("local_declaration", result.ast[1].kind)
+
+      local result2 = tl.process_string([[
+         local t = {
+            foo = "bar",
+            foo = "baz",
+         }
+      ]])
+      assert.same({}, result2.syntax_errors)
+      assert.same(1, #result2.ast)
+      assert.same(strip_typeids(result.ast), strip_typeids(result2.ast))
+   end)
+
+   it("records the fact that a table item has implicit key", function ()
+      local result = tl.process_string("return { ... }")
+      assert.same({}, result.syntax_errors)
+      assert.same("statements", result.ast.kind)
+      assert.same("return", result.ast[1].kind)
+      assert.same("table_literal", result.ast[1].exps[1].kind)
+      assert.same("table_item", result.ast[1].exps[1][1].kind)
+      assert.same("implicit", result.ast[1].exps[1][1].key_parsed)
+   end)
+
+   it("accepts nested type arguments", function ()
+      local result = tl.process_string([[
+         local record List<T>
+            items: {T}
+         end
+
+         local record Box<T>
+            item: {T}
+         end
+
+         local list_of_boxes: List<Box<string>> = {
+            items = {}
+         }
+
+         local box: Box<string> = { item = "hello" }
+
+         list_of_boxes.items[1] = box
+      ]])
+      assert.same({}, result.syntax_errors)
+      assert.same(5, #result.ast)
+      assert.same("local_type", result.ast[1].kind)
+      assert.same("local_type", result.ast[2].kind)
+      assert.same("local_declaration", result.ast[3].kind)
+      assert.same("local_declaration", result.ast[4].kind)
+      assert.same("assignment", result.ast[5].kind)
+   end)
+
+
+end)
